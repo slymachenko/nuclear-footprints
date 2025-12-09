21951993ea2cc106761bd410e99a638d32f81104
slymachenko <senya.s408@gmail.com>
2025-12-09 15:54:28 +0100

    env setup; python viz setup; file + code structure setup

---
+++ src/const.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent.parent
+
+DATA_DIR = ROOT_DIR / "data"
+
+ASSETS_DIR = ROOT_DIR / "assets"
+
+PAGES_DIR = ASSETS_DIR / "pages"
+
+PROTOCOLS_DIR = PAGES_DIR / "protocols"
+VISUALIZATIONS_DIR = PAGES_DIR / "visualizations"
